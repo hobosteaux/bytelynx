d5a0d01ffc3bf6efa60fa058c8479193ae999ae8
hobosteaux <theball89@gmail.com>
2014-05-20 20:47:28 -0700

    Retabbing + PEP8 ui file[s]

--- ui/menu.py
+++ ui/menu.py
@@ -1,63 +1,65 @@
 class MenuOption():
-	"""
-	Option for a menu.
+    """
+    Option for a menu.
 
-	.. attribute:: text
-		The text to display to the user.
-	.. attribute:: function
-		The function to execute.
-	"""
+    .. attribute:: text
+        The text to display to the user.
+    .. attribute:: function
+        The function to execute.
+    """
 
-	def __init__(self, text, function):
-		"""
-		:param text: Text to display.
-		:type text: str.
-		:param function: Function to execute on selection.
-		:type function: func()
-		"""
-		self.text = text
-		self.function = function
+    def __init__(self, text, function):
+        """
+        :param text: Text to display.
+        :type text: str.
+        :param function: Function to execute on selection.
+        :type function: func()
+        """
+        self.text = text
+        self.function = function
+
 
 class Menu():
-	"""
-	Simple wrapper for an interactive menu.
+    """
+    Simple wrapper for an interactive menu.
 
-	.. attribute:: options
-		A list of :class:`ui.MenuOption`s.
-	"""
+    .. attribute:: options
+        A list of :class:`ui.MenuOption`s.
+    """
 
-	def __init__(self, options):
-		"""
-		:param options: Options for the menu.
-		:type options: [:class:`ui.MenuOption`]
-		"""
-		self.options = options
+    def __init__(self, options):
+        """
+        :param options: Options for the menu.
+        :type options: [:class:`ui.MenuOption`]
+        """
+        self.options = options
 
-	def display(self):
-		"""
-		A while (True) wrapper for displaying options and letting the user choose one.
-		"""
-		while (True):
-			self.print()
-			choice = self.get_choice()
-			if (choice == len(self.options)):
-				break
-			else:
-				self.options[choice].function()
-			
-	def get_choice(self):
-		"""
-		Gets a choice from the user and checks it for validity.
-		"""
-		number = -1
-		while (number < 0) or (number > len(self.options)):
-			number = int(input('Enter your menu choice: '))
-		return number		
+    def display(self):
+        """
+        A while (True) wrapper for displaying options.
+        Lets the user choose one.
+        """
+        while (True):
+            self.print()
+            choice = self.get_choice()
+            if (choice == len(self.options)):
+                break
+            else:
+                self.options[choice].function()
 
-	def print(self):
-		"""
-		Displays the menu.
-		"""
-		for idx, item in enumerate(self.options):
-			print(idx, '-', item.text)
-		print(len(self.options), '- Exit')
+    def get_choice(self):
+        """
+        Gets a choice from the user and checks it for validity.
+        """
+        number = -1
+        while (number < 0) or (number > len(self.options)):
+            number = int(input('Enter your menu choice: '))
+        return number
+
+    def print(self):
+        """
+        Displays the menu.
+        """
+        for idx, item in enumerate(self.options):
+            print(idx, '-', item.text)
+        print(len(self.options), '- Exit')
